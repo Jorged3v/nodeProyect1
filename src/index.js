9045aa022215c2ec241b0bba4ddc3fe7d53f84af
Jorged3v <jorge.paz.d3v@gmail.com>
2022-11-08 06:35:34 -0500

    :sparkles: Config Express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+import express from 'express'
+import {dirname, join} from 'path'
+import {fileURLToPath} from 'url'
+
+const app = express();
+
+//Forma dinamica de obtener la ruta absoluta del fichero src
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+
+app.set('view engine', 'ejs');
+app.set('views', join(__dirname, 'views') )
+
+
+app.get ('/', (req, res) => res.render('index'));
+app.listen(3004);
+console.log('Server is litening on port: ', 3004);
